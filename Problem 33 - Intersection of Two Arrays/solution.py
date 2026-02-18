16793e7f37582b819b79b0573690162a83f4e087
JawadGigyani <jawadkhangigyani@gmail.com>
2026-02-18 21:48:51 +0500

    Problem 33 - Intersection of Two Arrays

---
+++ Problem 33 - Intersection of Two Arrays/solution.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        count = Counter(nums1)
+
+        result = []
+        
+        for num in nums2:
+            if count[num] > 0:
+                result.append(num)
+                count[num] = 0
+
+        return result 
