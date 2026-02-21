a1131c841b1a91cb2258952f135afc956d03997f
JawadGigyani <jawadkhangigyani@gmail.com>
2026-02-21 22:46:10 +0500

    Problem 38 - Max Consecutive Ones

---
+++ Problem 38 - Max Consecutive Ones/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        count = 0
+        max_ones = 0
+
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                count += 1
+            if nums[i] == 0:
+                count = 0
+
+            max_ones = max(max_ones, count)
+        return max_ones
