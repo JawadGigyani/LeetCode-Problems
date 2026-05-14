da5568bd3f574567bc502d17a735c92085bfe185
JawadGigyani <jawadkhangigyani@gmail.com>
2026-05-14 22:09:03 +0500

    Problem 112 - Minimum Add to Make Parentheses Valid

---
+++ Problem 112 - Minimum Add to Make Parentheses Valid/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        count = 0
+        stack = []
+        for ch in s:
+            if ch == '(':
+                stack.append(ch)
+            else:
+                if stack and stack[-1] == '(':
+                    stack.pop()
+                else:
+                    count += 1
+
+        return count + len(stack)
